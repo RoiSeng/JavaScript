91ca38927b866284d7043a0199f5e9f6ba61fdd2
RoiSeng <sengroi82@gmail.com>
2025-02-10 17:00:56 +0900

    news

---
+++ script.js
@@ -0,0 +1,78 @@
+const apiKey = 'c0553ec8c5e64fefbc128beea5a497e0';
+
+const blogContainer = document.getElementById("bolg-container");
+const searchField = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+async function fetchRandomNews() {
+    try {
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.error("Error fetching random news", error);
+        return [];
+    }
+}
+
+searchButton.addEventListener("click", async () => {
+    const query = searchField.value.trim(); // Corrected property name
+    if (query !== "") {
+        try {
+            const articles = await fetchNewsQuery(query);
+            displayBlogs(articles);
+        } catch (error) {
+            console.log("Error fetching news by query", error);
+        }
+    }
+});
+
+async function fetchNewsQuery(query) {
+    try {
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.error("Error fetching news query", error);
+        return [];
+    }
+}
+
+function displayBlogs(articles) {
+    blogContainer.innerHTML = ""; // Clears previous results
+    articles.forEach((article) => {
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt = article.title;
+
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+
+        const description = document.createElement("p");
+        description.textContent = article.description;
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+
+        blogCard.addEventListener('click', () => {
+            window.open(article.url, "_blank");
+        });
+
+        blogContainer.appendChild(blogCard); // Corrected the append statement
+    });
+}
+
+(async () => {
+    try {
+        const articles = await fetchRandomNews();
+        displayBlogs(articles);
+    } catch (error) {
+        console.error("Error fetching random news", error);
+    }
+})();
